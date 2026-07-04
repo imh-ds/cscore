18e7d57e991eee5be2f0e6e0b6a980238ffcaf1f
imh-ds <147938744+imh-ds@users.noreply.github.com>
2026-07-04 16:14:28 -0700

    Add HTMT cutoff and scale mismatch options

--- R/globals.R
+++ R/globals.R
@@ -1,20 +1,9 @@
-utils::globalVariables(
-  c(
-    ".",
-    "composite",
-    "indicator",
-    "loadings",
-    "weights",
-    "composite",
-    "alpha",
-    "rhoc",
-    "ave",
-    "loading_range",
-    "weight_range",
-    "median",
-    "Composite",
-    "from",
-    "to",
-    "variable"
-  )
-)
+# Silence R CMD check notes for tidy-eval column references used in dplyr verbs.
+utils::globalVariables(c(
+  "discriminant_validity",
+  "fornell_larcker",
+  "htmt",
+  "max_htmt",
+  "max_interconstruct_corr",
+  "sqrt_ave"
+))
